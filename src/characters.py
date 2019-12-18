6f1d3e07dd54eb2e8dfe47832bed1bf804947a4f
jordan <jchoi@ucvts.org>
2019-12-18 13:18:01 -0500

    create character class

---
+++ src/characters.py
@@ -0,0 +1,54 @@
+import sys
+import pygame
+
+#initialize pygame
+pygame.init()
+
+#create a display
+screen = pygame.display.set_mode( (700, 500) )
+print(pygame.QUIT)
+
+#create a background
+background = pygame.image.load("src/img/magnet-exterior.png")
+#define image size and location
+backrect = background.get_rect()
+#load image onto screen
+screen.blit(background, backrect)
+#update screen with changes
+pygame.display.flip()
+
+class Teachers:
+    """class of all collectable teachers"""
+    def __init__(self, name, goodie, cost, rarity):
+        """constructor for Teachers"""
+        self.name = name
+        self.goodie = goodie
+        self.cost = cost
+        self.rarity = rarity
+
+#all the teachers available to collect. Cost is based on year that they teach, and rarity is based on if they're still teaching here or not.
+teachers = [
+    Teachers('Mrs. Gerstein', 'Robot', 300, 'Common'),
+    Teachers('Mr. Sanservino', 'Pop Quiz', 2000, 'RARE'),
+    Teachers('Dr. Fang', 'Fang-mobile', 300, 'Common'),
+    Teachers('Mr. Weisser', 'Drum Set', 300, 'Common'),
+    Teachers('Mr. Stanko', 'AED', 300, 'Common'),
+    Teachers('Mr. Raite', 'Pure Oxygen', 300, 'Common'),
+    Teachers('Mr. Nowakoski', 'Fancy Knife', 8000, 'ULTRA RARE'),
+    Teachers('Mr. McMenamin', 'Patrick Star Shorts', 7000, 'ULTRA RARE'),
+    Teachers('Dr. Jidarian', 'GAs', 400, 'Common'),
+    Teachers('Ms. Valley', 'Deadlift', 200, 'Common'),
+    Teachers('Ms. Pinto', '35 Notecards', 200, 'Common'),
+    Teachers('Mrs. Kipp', 'AutoCAD Certificate', 8000, 'ULTRA RARE'),
+    Teachers('Mrs. Wickerhauser', 'Lab Procedures', 200, 'Common'),
+    Teachers('Mr. Moskowitz', 'Open Position', 600, 'Common')
+]
+
+
+# Main loop for game
+while True:
+    for event in pygame.event.get():
+        #check if current event is a quit event
+        if event.type == pygame.QUIT:
+            #quit program if the event is quit
+            sys.exit()
