f9fd0537837616239e12b9b6fa0864487e970c86
jordan <jchoi@ucvts.org>
2019-12-18 12:34:15 -0500

    loaded background image

--- src/window-setup.py
+++ src/window-setup.py
@@ -9,11 +9,11 @@ screen = pygame.display.set_mode( (700, 500) )
 print(pygame.QUIT)
 
 #create a background
-background = pygame.image.load("img/magnet-exterior.png")
-#fill window with color
-screen.fill( (255, 255, 255) )
-#fill screen with background
-screen.blit(background)
+background = pygame.image.load("src/img/magnet-exterior.png")
+#define image size and location
+backrect = background.get_rect()
+#load image onto screen
+screen.blit(background, backrect)
 #update screen with changes
 pygame.display.flip()
 
